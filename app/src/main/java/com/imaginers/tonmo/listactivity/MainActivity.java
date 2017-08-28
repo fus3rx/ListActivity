a2868929fd193ee81cc09e32a87fee372a74ba66
onirban <onirnab4385@diu.edu.bd>
2017-08-28 21:23:12 +0600

    Search option added to the list view

--- app/src/main/java/com/imaginers/tonmo/listactivity/MainActivity.java
+++ app/src/main/java/com/imaginers/tonmo/listactivity/MainActivity.java
@@ -3,22 +3,49 @@ package com.imaginers.tonmo.listactivity;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
     String[] mContacts ={"Rafi","Tusher","Ishayat","Shohag","Armaan"};
+    ArrayAdapter<String> adapter;
     ListView mContactList;
+    EditText inputSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputSearch = (EditText) findViewById(R.id.inputSearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                adapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+
         mContactList=getListView();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mContacts);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mContacts);
         mContactList.setAdapter(adapter);
 
     }
